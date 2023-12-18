7ac6064b6a34af2e14c330937b6d6acd1f27361c
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-12-18 20:40:02 -0300

    add authub starter project

---
+++ authub/src/main/kotlin/com/github/saviomisael/authub/AuthubApplication.kt
@@ -0,0 +1,11 @@
+package com.github.saviomisael.authub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthubApplication
+
+fun main(args: Array<String>) {
+	runApplication<AuthubApplication>(*args)
+}
---
+++ authub/src/test/kotlin/com/github/saviomisael/authub/AuthubApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.saviomisael.authub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthubApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
