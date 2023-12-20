ddd87fe55105873acf211da8887074ebf7c71f44
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-12-20 20:34:56 -0300

    implement saveChefCredentials

--- authub/src/main/kotlin/com/github/saviomisael/authub/adapter/infrastructure/repository/ChefRepository.kt
+++ authub/src/main/kotlin/com/github/saviomisael/authub/adapter/infrastructure/repository/ChefRepository.kt
@@ -3,13 +3,14 @@ package com.github.saviomisael.authub.adapter.infrastructure.repository
 import com.github.saviomisael.authub.adapter.infrastructure.persistence.ChefDtoRepository
 import com.github.saviomisael.authub.core.domain.entity.Chef
 import com.github.saviomisael.authub.core.domain.repository.IChefRepository
+import com.github.saviomisael.authub.shared.extensions.toChef
+import com.github.saviomisael.authub.shared.extensions.toChefDto
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Repository
 
 @Repository
 class ChefRepository(@Autowired val chefDtoRepository: ChefDtoRepository) : IChefRepository {
     override fun saveChefCredentials(chef: Chef): Chef {
-        TODO("Not yet implemented")
-        TODO("Create Builder and Adapter")
+        return chefDtoRepository.save(chef.toChefDto()).toChef()
     }
 }
