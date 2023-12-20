9cd41806fdff43aeede512d3c7718c959699f40a
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-12-20 19:46:19 -0300

    pass test should throw a EmptyStringException when email is empty

---
+++ authub/src/test/kotlin/com/github/saviomisael/authub/shared/builder/ChefBuilderTests.kt
@@ -0,0 +1,18 @@
+package com.github.saviomisael.authub.shared.builder
+
+import com.github.saviomisael.authub.shared.exceptions.EmptyStringException
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class ChefBuilderTests {
+    @Test
+    fun `should throw a EmptyStringException when email is empty`() {
+        Assertions.assertThatThrownBy {
+            ChefBuilder.createBuilder()
+                .withEmail("")
+                .build()
+        }
+            .isInstanceOf(EmptyStringException::class.java)
+            .hasMessage("Property email is empty.")
+    }
+}
