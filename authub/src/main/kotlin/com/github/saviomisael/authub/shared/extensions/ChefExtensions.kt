8b79e01ffef02fe1a829f404b94aa9636467d64a
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-12-20 20:12:18 -0300

    add ChefExtensions

---
+++ authub/src/main/kotlin/com/github/saviomisael/authub/shared/extensions/ChefExtensions.kt
@@ -0,0 +1,14 @@
+package com.github.saviomisael.authub.shared.extensions
+
+import com.github.saviomisael.authub.adapter.infrastructure.dto.ChefDto
+import com.github.saviomisael.authub.core.domain.entity.Chef
+import com.github.saviomisael.authub.shared.builder.ChefDtoBuilder
+
+fun Chef.toChefDto(): ChefDto {
+    return ChefDtoBuilder.createBuilder()
+        .withEmail(this.email)
+        .withPassword(this.password)
+        .withUsername(this.password)
+        .withFullName(this.fullName)
+        .build()
+}
