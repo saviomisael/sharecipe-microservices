aabe7926c13bbb5c5a98934b1572ac819150784f
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-12-20 21:15:43 -0300

    add UserDetailsAdapter

---
+++ authub/src/main/kotlin/com/github/saviomisael/authub/adapter/infrastructure/adapter/UserDetailsAdapter.kt
@@ -0,0 +1,21 @@
+package com.github.saviomisael.authub.adapter.infrastructure.adapter
+
+import com.github.saviomisael.authub.adapter.infrastructure.dto.ChefDto
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class UserDetailsAdapter(private val chefDto: ChefDto) : UserDetails {
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = mutableListOf()
+
+    override fun getPassword() = chefDto.password
+
+    override fun getUsername() = chefDto.username
+
+    override fun isAccountNonExpired() = true
+
+    override fun isAccountNonLocked() = true
+
+    override fun isCredentialsNonExpired() = true
+
+    override fun isEnabled() = true
+}
