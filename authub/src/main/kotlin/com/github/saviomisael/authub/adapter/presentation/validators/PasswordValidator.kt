be6144694d103b7f1a52da8dd087e7ca4a9b020c
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-12-23 16:23:27 -0300

    refactor PasswordValidator

--- authub/src/main/kotlin/com/github/saviomisael/authub/adapter/presentation/validators/PasswordValidator.kt
+++ authub/src/main/kotlin/com/github/saviomisael/authub/adapter/presentation/validators/PasswordValidator.kt
@@ -5,6 +5,11 @@ import jakarta.validation.ConstraintValidatorContext
 
 class PasswordValidator : ConstraintValidator<ValidPassword, String> {
     override fun isValid(password: String?, context: ConstraintValidatorContext?): Boolean {
-        return password != null && password.length >= 8 && password.any { it.isUpperCase() } && password.any { it.isLowerCase() } && password.any { it.isDigit() } && password.any { !it.isLetterOrDigit() }
+        return password != null
+                && password.length >= 8
+                && password.any { it.isUpperCase() }
+                && password.any { it.isLowerCase() }
+                && password.any { it.isDigit() }
+                && password.any { !it.isLetterOrDigit() }
     }
 }
