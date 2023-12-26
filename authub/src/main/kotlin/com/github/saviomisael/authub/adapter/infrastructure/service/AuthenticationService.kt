5856e8f0cdce5d5e1e0c69bd0dbe329920e3f8e4
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-12-26 20:24:04 -0300

    add AuthenticationService

---
+++ authub/src/main/kotlin/com/github/saviomisael/authub/adapter/infrastructure/service/AuthenticationService.kt
@@ -0,0 +1,11 @@
+package com.github.saviomisael.authub.adapter.infrastructure.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.stereotype.Service
+
+@Service
+class AuthenticationService @Autowired constructor(private val authenticationManager: AuthenticationManager) {
+    fun authenticate(username: String, password: String) = authenticationManager.authenticate(UsernamePasswordAuthenticationToken(username, password))
+}
