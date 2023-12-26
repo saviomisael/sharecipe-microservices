73456fd228afce52511c2324e3f7b217f0be7633
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-12-26 20:59:07 -0300

    refactor BaseController

--- authub/src/main/kotlin/com/github/saviomisael/authub/adapter/presentation/controller/BaseController.kt
+++ authub/src/main/kotlin/com/github/saviomisael/authub/adapter/presentation/controller/BaseController.kt
@@ -3,14 +3,8 @@ package com.github.saviomisael.authub.adapter.presentation.controller
 import org.springframework.http.ResponseEntity
 
 abstract class BaseController {
-    fun <T> created(body: T): ResponseEntity<T> {
-        return ResponseEntity.status(201).body(body)
-    }
-
-    fun <T> unprocessableEntity(body: T): ResponseEntity<T> {
-        return ResponseEntity.status(422).body(body)
-    }
-
+    fun <T> created(body: T): ResponseEntity<T> = ResponseEntity.status(201).body(body)
+    fun <T> unprocessableEntity(body: T): ResponseEntity<T> = ResponseEntity.status(422).body(body)
     fun <T> unauthorized(body: T): ResponseEntity<T> = ResponseEntity.status(401).body(body)
     fun <T> ok(body: T): ResponseEntity<T> = ResponseEntity.status(200).body(body)
 }
