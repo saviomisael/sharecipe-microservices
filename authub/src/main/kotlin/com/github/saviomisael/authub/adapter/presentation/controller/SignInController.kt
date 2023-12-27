0bc95f308a0b7cf8e212e73fed97c9285df24ef9
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-12-26 21:06:19 -0300

    add swagger documentation for SignInController

--- authub/src/main/kotlin/com/github/saviomisael/authub/adapter/presentation/controller/SignInController.kt
+++ authub/src/main/kotlin/com/github/saviomisael/authub/adapter/presentation/controller/SignInController.kt
@@ -6,6 +6,11 @@ import com.github.saviomisael.authub.adapter.presentation.v1.ApiRoutes
 import com.github.saviomisael.authub.core.domain.dto.TokenResultDto
 import com.github.saviomisael.authub.core.domain.usecases.ISignInUseCase
 import com.github.saviomisael.authub.shared.exceptions.CredentialsInvalidException
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
@@ -15,6 +20,18 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class SignInController @Autowired constructor(private val signInUseCase: ISignInUseCase) : BaseController() {
+    @Operation(summary = "Sign in into sharecipe", description = "Returns 200 if successfully")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "Return your JWT token."),
+            ApiResponse(responseCode = "401", description = "Username or password are wrong."),
+            ApiResponse(
+                responseCode = "400",
+                description = "Validation failed for the body. It returns a Map<String, String>",
+                content = [Content(mediaType = "application/json", schema = Schema(implementation = Map::class))]
+            )
+        ]
+    )
     @PostMapping(ApiRoutes.ChefRoutes.signIn)
     fun signIn(@Valid @RequestBody dto: SignInCredentialsDto): ResponseEntity<ResponseDto<TokenResultDto>> {
         try {
