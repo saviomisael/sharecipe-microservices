3f75fd92e39cd4adc5312c0450db985075c8acfd
Savio Moreira <misaelmoreira99.sm@gmail.com>
2024-01-01 18:24:40 -0300

    pass tests for Scenario: The new password is fewer than 8 characters

--- authub/src/test/kotlin/com/github/saviomisael/authub/steps/ChangePasswordSteps.kt
+++ authub/src/test/kotlin/com/github/saviomisael/authub/steps/ChangePasswordSteps.kt
@@ -2,7 +2,9 @@ package com.github.saviomisael.authub.steps
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.saviomisael.authub.adapter.presentation.dto.ChangePasswordDto
+import com.github.saviomisael.authub.adapter.presentation.dto.CreateChefDto
 import com.github.saviomisael.authub.adapter.presentation.v1.ApiRoutes
+import io.cucumber.java.en.And
 import io.cucumber.java.en.Given
 import io.cucumber.java.en.Then
 import io.cucumber.java.en.When
@@ -15,11 +17,42 @@ import org.springframework.http.HttpStatus
 class ChangePasswordSteps {
   private val objectMapper = ObjectMapper()
   private var token = ""
+  private var newPassword = ""
   private lateinit var performRequest: ValidatableResponse
 
   @Given("A user without a token")
   fun `A user without a token`() {
     token = ""
+  }
+
+  @Given("A chef that creates his account")
+  fun `A chef that creates his account`() {
+    token = RestAssured
+      .given()
+      .log()
+      .all()
+      .contentType(ContentType.JSON)
+      .body(
+        objectMapper.writeValueAsString(
+          CreateChefDto(
+            "User wants change password",
+            "userwantschangepassword",
+            "@Test123",
+            "userwantschangepassword@email.com"
+          )
+        )
+      )
+      .`when`()
+      .post(ApiRoutes.ChefRoutes.createChefAccount)
+      .then()
+      .extract()
+      .jsonPath()
+      .getString("data.token")
+  }
+
+  @And("Wants to change his password with one that is less than 8 characters")
+  fun `Wants to change his password with one that is less than 8 characters`() {
+    newPassword = "123"
   }
 
   @When("He tries to change his password")
@@ -28,7 +61,9 @@ class ChangePasswordSteps {
       .given()
       .log()
       .all()
-      .body(objectMapper.writeValueAsString(ChangePasswordDto("@Test123")))
+      .contentType(ContentType.JSON)
+      .header("Authorization", "Bearer $token")
+      .body(objectMapper.writeValueAsString(ChangePasswordDto(newPassword)))
       .`when`()
       .patch(ApiRoutes.ChefRoutes.changePasswords)
       .then()
@@ -48,5 +83,14 @@ class ChangePasswordSteps {
         .getString("errors")
 
     Assertions.assertThat(errors).isNotEqualTo("[]")
+  }
+
+  @Then("Returns a bad request for invalid password")
+  fun `Returns a bad request for invalid password`() {
+    performRequest
+      .log()
+      .all()
+      .contentType(ContentType.JSON)
+      .statusCode(HttpStatus.BAD_REQUEST.value())
   }
 }
--- authub/src/test/resources/com/github/saviomisael/authub/features/change_password.feature
+++ authub/src/test/resources/com/github/saviomisael/authub/features/change_password.feature
@@ -1,7 +1,13 @@
 Feature: Change password for a chef
-  As a logged-in chef, I want to be able to change my password
+  As a logged-in chef or a new user, I want to be able to change my password
 
   Scenario: The chef is not logged-in
     Given A user without a token
     When He tries to change his password
     Then Returns unauthorized because he is not logged-in
+
+  Scenario: The new password is fewer than 8 characters
+    Given A chef that creates his account
+    And Wants to change his password with one that is less than 8 characters
+    When He tries to change his password
+    Then Returns a bad request for invalid password
