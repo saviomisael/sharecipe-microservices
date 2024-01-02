0d694fc53d8f5d1fea93febde50591e099a69e97
Savio Moreira <misaelmoreira99.sm@gmail.com>
2024-01-02 20:00:24 -0300

    remove swagger from qa environment

--- authub/src/main/kotlin/com/github/saviomisael/authub/adapter/infrastructure/config/SwaggerConfig.kt
+++ authub/src/main/kotlin/com/github/saviomisael/authub/adapter/infrastructure/config/SwaggerConfig.kt
@@ -4,10 +4,8 @@ import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 
 @Configuration
-@Profile("dev")
 class SwaggerConfig {
   @Bean
   fun openAPI(): OpenAPI {
