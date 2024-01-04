7151246914d7c5fc6105638a17db75e65b539b82
Savio Moreira <misaelmoreira99.sm@gmail.com>
2024-01-03 21:42:32 -0300

    add getBody for HttpServletRequest

--- authub/src/main/kotlin/com/github/saviomisael/authub/shared/extensions/HttpServletRequestExtensions.kt
+++ authub/src/main/kotlin/com/github/saviomisael/authub/shared/extensions/HttpServletRequestExtensions.kt
@@ -1,5 +1,8 @@
 package com.github.saviomisael.authub.shared.extensions
 
 import jakarta.servlet.http.HttpServletRequest
+import java.util.stream.Collectors
 
 fun HttpServletRequest.getUsername() = this.getAttribute("username").toString()
+
+fun HttpServletRequest.getBody() = this.reader.lines().collect(Collectors.joining())
