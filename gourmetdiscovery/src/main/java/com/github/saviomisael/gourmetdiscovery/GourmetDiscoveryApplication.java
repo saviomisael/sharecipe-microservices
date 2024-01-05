d34d975cfa748d2cb8766df5ebc291b76d5fb837
Savio Moreira <misaelmoreira99.sm@gmail.com>
2024-01-04 22:44:39 -0300

    add eureka server configuration

--- gourmetdiscovery/src/main/java/com/github/saviomisael/gourmetdiscovery/GourmetDiscoveryApplication.java
+++ gourmetdiscovery/src/main/java/com/github/saviomisael/gourmetdiscovery/GourmetDiscoveryApplication.java
@@ -2,12 +2,14 @@ package com.github.saviomisael.gourmetdiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class GourmetdiscoveryApplication {
+@EnableEurekaServer
+public class GourmetDiscoveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GourmetdiscoveryApplication.class, args);
+		SpringApplication.run(GourmetDiscoveryApplication.class, args);
 	}
 
 }
