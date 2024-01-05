353768471e0aa5c6aa0d0241eefe61f2bcdf2128
Savio Moreira <misaelmoreira99.sm@gmail.com>
2024-01-04 22:31:29 -0300

    add initial gourmetdiscovery

---
+++ gourmetdiscovery/src/main/java/com/github/saviomisael/gourmetdiscovery/GourmetdiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.github.saviomisael.gourmetdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GourmetdiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GourmetdiscoveryApplication.class, args);
+	}
+
+}
