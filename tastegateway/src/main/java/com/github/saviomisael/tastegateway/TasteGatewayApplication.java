91779ed2a51643a5a22ecd7a47702685c5488c7d
Savio Moreira <misaelmoreira99.sm@gmail.com>
2024-01-05 10:37:09 -0300

    add initial taste gateway project

---
+++ tastegateway/src/main/java/com/github/saviomisael/tastegateway/TasteGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.github.saviomisael.tastegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasteGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasteGatewayApplication.class, args);
+	}
+
+}
