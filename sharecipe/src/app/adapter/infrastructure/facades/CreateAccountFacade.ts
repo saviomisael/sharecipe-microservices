06f6ab0220d5e68e000386409f2dc0e1f5aa62f2
Savio Moreira <misaelmoreira99.sm@gmail.com>
2024-01-08 15:37:39 -0300

    add CreateAccountFacade

---
+++ sharecipe/src/app/adapter/infrastructure/facades/CreateAccountFacade.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CreateAccountResponseDto } from '../http/responses/CreateAccountResponseDto';
+import { CreateAccountDispatcher } from '../store/dispatchers/CreateAccountDispatcher';
+import { CreateAccountErrorHandler } from '../store/handlers/CreateAccountErrorHandler';
+
+@Injectable()
+export class CreateAccountFacade {
+  constructor(
+    private createAccountDispatcher: CreateAccountDispatcher,
+    private createAccountErrorHandler: CreateAccountErrorHandler
+  ) {}
+
+  createAccount({
+    expiresAt,
+    username,
+    token,
+  }: Omit<CreateAccountResponseDto, 'fullName'>) {
+    this.createAccountDispatcher.dispatch({ expiresAt, token, username });
+  }
+
+  showErrors(errors: string[]) {
+    this.createAccountErrorHandler.handle(errors);
+  }
+}
