fdc20ca17122bfce406ef8cac219f6240cf69e06
Savio Moreira <misaelmoreira99.sm@gmail.com>
2024-01-08 15:34:53 -0300

    add LocalStorageService

---
+++ sharecipe/src/app/adapter/infrastructure/services/LocalStorageService.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class LocalStorageService {
+  saveCurrentUser(token: string, expiresAt: string, username: string) {
+    localStorage.setItem('t', btoa(token));
+    localStorage.setItem('e', btoa(expiresAt));
+    localStorage.setItem('u', btoa(username));
+  }
+
+  getCurrentUser() {
+    const token = localStorage.getItem('t');
+    const expiresAt = localStorage.getItem('e');
+    const username = localStorage.getItem('u');
+
+    if (!token || !expiresAt || !username)
+      return { token: null, expiresAt: null, username: null };
+
+    return {
+      token: atob(token),
+      expiresAt: atob(expiresAt),
+      username: atob(username),
+    };
+  }
+}
