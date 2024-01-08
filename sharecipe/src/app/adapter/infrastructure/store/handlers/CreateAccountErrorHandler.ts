1e8e755075f392719009b6df89e9b76801770fd2
Savio Moreira <misaelmoreira99.sm@gmail.com>
2024-01-08 15:37:03 -0300

    add CreateAccountErrorHandler

---
+++ sharecipe/src/app/adapter/infrastructure/store/handlers/CreateAccountErrorHandler.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { showCreateAccountErrors } from '../actions/account.actions';
+import { IHandler } from './IHandler';
+
+@Injectable()
+export class CreateAccountErrorHandler implements IHandler<string[]> {
+  constructor(private store: Store) {}
+
+  handle(params: string[]): void {
+    this.store.dispatch(showCreateAccountErrors({ errors: params }));
+  }
+}
