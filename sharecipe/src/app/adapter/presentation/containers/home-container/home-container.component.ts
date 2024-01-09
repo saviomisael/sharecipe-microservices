f006653d18cd24b26cb0892a4d2d323346f29adb
Savio Moreira <misaelmoreira99.sm@gmail.com>
2024-01-09 13:27:28 -0300

    add HomeContainer

---
+++ sharecipe/src/app/adapter/presentation/containers/home-container/home-container.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-home-container',
+  templateUrl: './home-container.component.html',
+  styleUrl: './home-container.component.scss'
+})
+export class HomeContainerComponent {
+
+}
