f01b83af2196735092dfbb34a2132c9d413707d5
Savio Moreira <misaelmoreira99.sm@gmail.com>
2024-01-09 12:26:06 -0300

    add logout handler for MainHeaderWrapper

--- sharecipe/src/app/adapter/presentation/containers/main-header-wrapper/main-header-wrapper.component.ts
+++ sharecipe/src/app/adapter/presentation/containers/main-header-wrapper/main-header-wrapper.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {Store} from "@ngrx/store";
 import {selectUsername} from "../../../infrastructure/store/selectors/account.selectors";
 import {Observable} from "rxjs";
+import {Router} from "@angular/router";
+import {LogoutDispatcher} from "../../../infrastructure/store/dispatchers/LogoutDispatcher";
 
 @Component({
   selector: 'app-main-header-wrapper',
@@ -9,10 +11,15 @@ import {Observable} from "rxjs";
   styleUrl: './main-header-wrapper.component.scss'
 })
 export class MainHeaderWrapperComponent {
-  constructor(private store: Store) {
+  constructor(private store: Store, private router: Router, private logoutDispatcher: LogoutDispatcher) {
   }
 
   get username$(): Observable<string> {
     return this.store.select(selectUsername)
+  }
+
+  handleLogoutClick() {
+    this.logoutDispatcher.dispatch()
+    this.router.navigate(['/login'])
   }
 }
